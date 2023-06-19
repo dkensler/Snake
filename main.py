339e4793edd40dac97e7c8eeb9c0259b2a7cec72
Dylan Kensler <dylankensler@Dylans-MacBook-Air.local>
2023-06-18 21:40:54 -0400

    SNK-1: Creates the pygame window  - Quits upon clicking 'X'

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+pygame.init()
+mainWindow = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Snake")
+
+quitWindow = False
+
+while not quitWindow:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            quitWindow = True
+
+pygame.quit()
+quit()
